db1a6f5b30515a2a70e3ddaf69e39b6a498add5c
Jakub Adler <adler.jakub@gmail.com>
2017-08-02 13:18:11 +0100

    Extracting role revealing variable

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -5,7 +5,9 @@ class BitmapEditor
 
   def run(command)
     args = command.split(' ')
-    case args[0]
+    type_of_command = args[0]
+
+    case type_of_command
     when 'I'
       @image.create(width: args[1].to_i, height: args[2].to_i)
     when 'C'
