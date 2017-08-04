2a31cd5cce4e28ba2329bbce1163a1e1b9e4b45d
Jakub Adler <adler.jakub@gmail.com>
2017-08-04 13:45:53 +0100

    Moving BitmapEditor run method to subject

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -3,23 +3,31 @@ require_relative '../app/bitmap_editor'
 
 describe BitmapEditor do
   let(:image) { double :image }
+  let(:bitmap_editor) { described_class.new(image) }
+  let(:command) { '' }
+
+  subject(:run_command) { bitmap_editor.run(command) }
+
   describe 'I 0 0' do
     it 'does nothing'
   end
 
   describe 'I 1 1' do
+    let(:command) { 'I 1 1' }
+
     it 'creates and 1 x 1 image' do
       expect(image).to receive(:create).with(width: 1, height: 1)
 
-      BitmapEditor.new(image).run('I 1 1')
+      run_command
     end
   end
 
   describe 'I 2 3' do
+    let(:command) { 'I 2 3' }
     it 'creates and 2 x 3 image' do
       expect(image).to receive(:create).with(width: 2, height: 3)
 
-      BitmapEditor.new(image).run('I 2 3')
+      run_command
     end
   end
 
@@ -32,10 +40,12 @@ describe BitmapEditor do
   end
 
   describe 'C' do
+    let(:command) { 'C' }
+
     it 'clears image' do
       expect(image).to receive(:clear)
 
-      BitmapEditor.new(image).run('C')
+      run_command
     end
   end
 
@@ -44,10 +54,12 @@ describe BitmapEditor do
   end
 
   describe 'L 1 1 B' do
+    let(:command) { 'L 1 1 B' }
+
     it 'Colours fist pixel in row and first pixel in column to black' do
       expect(image).to receive(:color_pixel).with(x: 1, y: 1, color: 'B')
 
-      BitmapEditor.new(image).run('L 1 1 B')
+      run_command
     end
   end
 end
