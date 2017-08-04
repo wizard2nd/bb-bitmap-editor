8f4d81bdce6d5df9c8f441c53887d1d37b2a422d
Jakub Adler <adler.jakub@gmail.com>
2017-08-04 13:35:36 +0100

    Drying up specs by placing image to let variable

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -2,13 +2,13 @@ require 'rspec'
 require_relative '../app/bitmap_editor'
 
 describe BitmapEditor do
+  let(:image) { double :image }
   describe 'I 0 0' do
     it 'does nothing'
   end
 
   describe 'I 1 1' do
     it 'creates and 1 x 1 image' do
-      image = double :image
       expect(image).to receive(:create).with(width: 1, height: 1)
 
       BitmapEditor.new(image).run('I 1 1')
@@ -17,7 +17,6 @@ describe BitmapEditor do
 
   describe 'I 2 3' do
     it 'creates and 2 x 3 image' do
-      image = double :image
       expect(image).to receive(:create).with(width: 2, height: 3)
 
       BitmapEditor.new(image).run('I 2 3')
@@ -34,7 +33,6 @@ describe BitmapEditor do
 
   describe 'C' do
     it 'clears image' do
-      image = double :image
       expect(image).to receive(:clear)
 
       BitmapEditor.new(image).run('C')
@@ -47,7 +45,6 @@ describe BitmapEditor do
 
   describe 'L 1 1 B' do
     it 'Colours fist pixel in row and first pixel in column to black' do
-      image = double :image
       expect(image).to receive(:color_pixel).with(x: 1, y: 1, color: 'B')
 
       BitmapEditor.new(image).run('L 1 1 B')
