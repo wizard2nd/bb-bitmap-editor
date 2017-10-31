f452ce54cae927ae88c4d971577e07aa888ecc0e
Jakub Adler <adler.jakub@gmail.com>
2017-10-31 13:07:51 +0000

    Create point to draw on right place

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -6,8 +6,6 @@ class BitmapEditor
   def run(command)
     args = command.upcase.split(' ')
     type_of_command = args[0]
-    point = { x: args[1].to_i.abs, y: args[2].to_i.abs }
-    point = OpenStruct.new(point)
 
     case type_of_command
     when 'I'
@@ -15,6 +13,8 @@ class BitmapEditor
     when 'C'
       @image.clear
     when 'L'
+      point = { x: args[1].to_i.abs, y: args[2].to_i.abs }
+      point = OpenStruct.new(point)
       @image.color_pixel(point: point, color: args[3])
     when 'V'
       @image.color_vertical_line(x: args[1].to_i, y1: args[2].to_i, y2: args[3].to_i, color: args[4])
